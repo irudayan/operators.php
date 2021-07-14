851465edf3b18196109ca0833eca87e16e26e317
irudayan <irudayan111@gmail.com>
2021-07-14 17:03:33 +0530

    first

--- assigm.php
+++ assigm.php
@@ -8,7 +8,28 @@
 $a = 20;
 $a -= 40;
 echo $a;
+echo '<br>';
 
+$x = -60;
+echo $x;
+echo '<br>';
+
+$b = 80;
+$b *= 10;
+echo $b;
+echo '<br>';
+
+
+$s = 90;
+$s += 50;
+echo $s;
+echo '<br>';
+
+
+$n = 20;
+$n -= 10;
+echo $n;
+echo '<br>';
 
 
 ?>
