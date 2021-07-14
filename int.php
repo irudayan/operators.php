6b83c658053b22932a7888332c2237f2296ff135
irudayan <irudayan111@gmail.com>
2021-07-14 14:29:19 +0530

    first commit

---
+++ int.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<?php
+$txt = "Hello world!";
+ $x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+</body>
+</html>
---
+++ operators.php
@@ -0,0 +1,37 @@
+<html>
+<body>
+
+<?php
+$x = 10;  
+$y = 6;
+
+$c = $x + $y;
+echo $c;
+echo "<br>";
+
+$d = $x - $y;
+echo $d;
+echo "<br>";
+
+$e = $x * $y;
+echo $e;
+echo "<br>";
+
+$f =$x / $y;
+echo $f;
+echo "<br>";
+
+$g =$x ** $y;
+echo $g;
+echo "<br>";
+
+$h = $x % $y;
+echo $h;
+
+
+
+
+?>  
+
+</body>
+</html>
