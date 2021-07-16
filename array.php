0ba1eb7806f2c71aaff6e1b862fea110b08cd924
irudayan <irudayan111@gmail.com>
2021-07-16 17:36:22 +0530

    statement

---
+++ array.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>array</title>
+
+<style>
+body
+{
+background-color="pink";
+}
+</style>
+<body>
+<?php
+$family = array("Dias","Vincent","Raj","George","Kumar");
+
+echo "My elder brother name:"."$family[0]";
+echo "<br>";
+echo "My elder brother name:"."$family[1]";
+echo "<br>";
+
+echo "It's me:"."$family[2]";
+echo "<br>";
+
+echo "My younger brother name:"."$family[3]";
+echo "<br>";
+
+echo "My younger brother name:"."$family[4]";
+echo "<br>";
+
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
--- statement.php
+++ statement.php
@@ -1,8 +1,40 @@
-<html>
+<html>:
 <body>
 <?php
-$t = 
-0
+
+$day = date('w'); 
+
+if ($day = 0)
+{
+	echo "today sunday";
+}
+else if ($day = 1)
+{
+	echo "today monday";
+}
+else if ($day = 2)
+{
+	echo "today tuesday";
+}
+else if ($day = 3)
+{
+	echo "today wednesday";
+}
+else if ($day = 4)
+{	
+	echo "today thruesday";
+}
+else if ($day = 5)
+{
+echo "today friday";
+}
+else
+{
+echo "today saturday";
+}
 ?>
+
+
+
 </body>
 </html>
